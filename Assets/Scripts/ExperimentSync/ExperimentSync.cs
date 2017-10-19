4d5e9396d1cba48727b12ab1bec3d86ad99f4afb
Erwan Normand <normand.erwan@protonmail.com>
2017-10-19 14:30:26 -0400

    Added a ExperimentSync script and prefab to sync experiment current state, conditions and task between the server and the clients.

---
+++ Assets/Scripts/ExperimentSync/ExperimentSync.cs
@@ -0,0 +1,101 @@
+﻿using DevicesSyncUnity;
+using DevicesSyncUnity.Messages;
+using System;
+using UnityEngine.Networking;
+
+namespace NormandErwan.MasterThesisExperiment.States
+{
+    /// <summary>
+    /// Synchronize the experiment between devices with <see cref="StateMessage"/>.
+    /// </summary>
+    public class ExperimentSync : DevicesSync
+    {
+        // Properties
+
+        /// <summary>
+        /// Gets the current synchronized state.
+        /// </summary>
+        public State State { get { return latestStateMessage.state; } set { SendNewState(value); } }
+
+        // Events
+
+        /// <summary>
+        /// Called on server and on device client when <see cref="State"/> has been updated by a device.
+        /// </summary>
+        public event Action<StateMessage> StateUpdated = delegate { };
+
+        // Variables
+
+        protected StateMessage latestStateMessage = new StateMessage();
+        protected StateMessage sendStateMessage = new StateMessage();
+
+        // Methods
+
+        /// <summary>
+        /// Initializes the properties.
+        /// </summary>
+        protected override void Awake()
+        {
+            base.Awake();
+
+            MessageTypes.Add(latestStateMessage.MessageType);
+        }
+
+        /// <summary>
+        /// Send to other devices the new state.
+        /// </summary>
+        /// <param name="newState">The new state to synchronise.</param>
+        protected virtual void SendNewState(State newState)
+        {
+            if (newState != latestStateMessage.state)
+            {
+                sendStateMessage.state = newState;
+                SendToServer(sendStateMessage);
+            }
+        }
+
+        /// <summary>
+        /// Calls <see cref="ProcessMessageReceived(NetworkMessage)"/>.
+        /// </summary>
+        protected override DevicesSyncMessage OnServerMessageReceived(NetworkMessage netMessage)
+        {
+            return ProcessMessageReceived(netMessage);
+        }
+
+        /// <summary>
+        /// Calls <see cref="ProcessMessageReceived(NetworkMessage)"/>.
+        /// </summary>
+        protected override DevicesSyncMessage OnClientMessageReceived(NetworkMessage netMessage)
+        {
+            return ProcessMessageReceived(netMessage);
+        }
+
+        /// <summary>
+        /// Updates <see cref="State"/> and invokes <see cref="StateUpdated"/>.
+        /// </summary>
+        /// <param name="netMessage">The received networking message.</param>
+        /// <returns>The typed network message extracted.</returns>
+        protected virtual DevicesSyncMessage ProcessMessageReceived(NetworkMessage netMessage)
+        {
+            latestStateMessage = netMessage.ReadMessage<StateMessage>();
+            StateUpdated.Invoke(latestStateMessage);
+            return latestStateMessage;
+        }
+
+        /// <summary>
+        /// See <see cref="DevicesSync.OnClientDeviceConnected(int)"/>.
+        /// </summary>
+        /// <param name="deviceId"></param>
+        protected override void OnClientDeviceConnected(int deviceId)
+        {
+            SendToClient(deviceId, latestStateMessage);
+        }
+
+        /// <summary>
+        /// See <see cref="DevicesSync.OnClientDeviceDisconnected(int)"/>.
+        /// </summary>=
+        protected override void OnClientDeviceDisconnected(int deviceId)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/MessageType.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine.Networking;
+
+namespace NormandErwan.MasterThesisExperiment
+{
+    /// <summary>
+    /// Container class for networking messages types used in this experiment.
+    /// </summary>
+    public class MessageType : DevicesSyncUnity.Messages.MessageType
+    {
+        // Constants
+
+        /// <summary>
+        /// See <see cref="DevicesSyncUnity.Messages.MessageType.Smallest"/>.
+        /// </summary>
+        public static new short Smallest { get { return smallest; } set { smallest = value; } }
+
+        /// <summary>
+        /// Networking message for communicating <see cref="States.StateMessage"/>
+        /// </summary>
+        public static short State { get { return (short)(Smallest + 1); } }
+
+        /// <summary>
+        /// See <see cref="DevicesSyncUnity.Messages.MessageType.Highest"/>.
+        /// </summary>
+        public static new short Highest { get { return State; } }
+
+        // Variables
+
+        private static short smallest = (short)(DevicesSyncUnity.Examples.Messages.MessageType.Highest + 1);
+
+        // Methods
+
+        /// <summary>
+        /// See <see cref="MsgType.MsgTypeToString(short)"/>.
+        /// </summary>
+        public static new string MsgTypeToString(short value)
+        {
+            if (value == State)
+            {
+                return "State";
+            }
+            else
+            {
+                return MsgType.MsgTypeToString(value);
+            }
+        }
+    }
+}
