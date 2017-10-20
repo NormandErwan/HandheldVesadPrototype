ebffdb64ae0795ea4516f0778b7d6d42edce16c8
Erwan Normand <normand.erwan@protonmail.com>
2017-10-20 01:12:05 -0400

    Added a field 'instructions' on IV Technique.cs

--- Assets/Scripts/Variables/Technique.cs
+++ Assets/Scripts/Variables/Technique.cs
@@ -1,6 +1,12 @@
-﻿namespace NormandErwan.MasterThesisExperiment.Variables
+﻿using UnityEngine;
+
+namespace NormandErwan.MasterThesisExperiment.Variables
 {
     public class Technique : IndependantVariable
     {
+        // Editor Fields
+
+        [TextArea]
+        public string instructions;
     }
 }
