8d7ed6f1574bbd2378e0941188c8804153f84109
Erwan Normand <normand.erwan@protonmail.com>
2017-12-05 09:49:43 -0500

    Improved ArucoLeapHandsAlignement.cs

--- Assets/Scripts/Inputs/ArucoLeapHandsAlignment.cs
+++ Assets/Scripts/Inputs/ArucoLeapHandsAlignment.cs
@@ -5,7 +5,7 @@ namespace NormandErwan.MasterThesisExperiment.Utilities
 {
   /// <summary>
   /// Aligns the <see cref="leapHandController"/> with the aruco coordinates: point your right index <see cref="leapHandRight_IndexEnd"/> to the
-  /// <see cref="targetSphere"/> that is a child of an Aruco Object and press the <see cref="calibrateKey"/> to update the position of the leap hand
+  /// <see cref="targetSphere"/> that is a child of an Aruco Object and press the <see cref="aligningKey"/> to update the position of the leap hand
   /// controller.
   /// </summary>
   public class ArucoLeapHandsAlignment : MonoBehaviour
@@ -22,12 +22,14 @@ namespace NormandErwan.MasterThesisExperiment.Utilities
     private Transform targetSphere;
 
     [SerializeField]
-    private KeyCode calibrateKey = KeyCode.C;
+    private KeyCode aligningKey = KeyCode.C;
 
     [SerializeField]
     private Vector3 defaultLeapHandControllerOffset = new Vector3(0.0014f, -0.0412f, -0.0115f);
 
     // Properties
+
+    public bool Aligning { get; protected set; }
 
     public Vector3 LeapHandArucoMarkerPositionOffset { get; protected set; }
 
@@ -40,7 +42,12 @@ namespace NormandErwan.MasterThesisExperiment.Utilities
 
     protected virtual void Update()
     {
-      if (Input.GetKeyUp(calibrateKey))
+      if (Input.GetKeyUp(aligningKey))
+      {
+        Aligning = !Aligning;
+      }
+
+      if (Aligning)
       {
         leapHandController.transform.position -= LeapHandArucoMarkerPositionOffset;
         LeapHandArucoMarkerPositionOffset = targetSphere.position - leapHandRight_IndexEnd.position;
