c8072a9d13b87be53875bab5efb7b51d3443af65
Erwan Normand <normand.erwan@protonmail.com>
2017-12-04 20:07:53 -0500

    Added a ArucoLeapAlignement script that is good enough for now.

---
+++ Assets/Scripts/Inputs/ArucoLeapHandsAlignment.cs
@@ -0,0 +1,51 @@
+﻿using Leap.Unity;
+using UnityEngine;
+
+namespace NormandErwan.MasterThesisExperiment.Utilities
+{
+  /// <summary>
+  /// Aligns the <see cref="leapHandController"/> with the aruco coordinates: point your right index <see cref="leapHandRight_IndexEnd"/> to the
+  /// <see cref="targetSphere"/> that is a child of an Aruco Object and press the <see cref="calibrateKey"/> to update the position of the leap hand
+  /// controller.
+  /// </summary>
+  public class ArucoLeapHandsAlignment : MonoBehaviour
+  {
+    // Editor fields
+
+    [SerializeField]
+    private LeapHandController leapHandController;
+
+    [SerializeField]
+    private Transform leapHandRight_IndexEnd;
+
+    [SerializeField]
+    private Transform targetSphere;
+
+    [SerializeField]
+    private KeyCode calibrateKey = KeyCode.C;
+
+    [SerializeField]
+    private Vector3 defaultLeapHandControllerOffset = new Vector3(0.0014f, -0.0412f, -0.0115f);
+
+    // Properties
+
+    public Vector3 LeapHandArucoMarkerPositionOffset { get; protected set; }
+
+    // Methods
+
+    protected virtual void Awake()
+    {
+      LeapHandArucoMarkerPositionOffset = defaultLeapHandControllerOffset;
+    }
+
+    protected virtual void Update()
+    {
+      if (Input.GetKeyUp(calibrateKey))
+      {
+        leapHandController.transform.position -= LeapHandArucoMarkerPositionOffset;
+        LeapHandArucoMarkerPositionOffset = targetSphere.position - leapHandRight_IndexEnd.position;
+        leapHandController.transform.position += LeapHandArucoMarkerPositionOffset;
+      }
+    }
+  }
+}
