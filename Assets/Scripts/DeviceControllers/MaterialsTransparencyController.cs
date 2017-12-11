1681aef2e0ce696d1a8079f0e1c21caa40b94e66
Erwan Normand <normand.erwan@protonmail.com>
2017-12-11 01:04:28 -0500

    Added UnlitBackground.shader, a simple unlit with one color shader, and MaterialsTransparencyController.cs associated with the DeviceController.cs

---
+++ Assets/Scripts/DeviceControllers/MaterialsTransparencyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace NormandErwan.MasterThesis.Experiment.DeviceControllers
+{
+  public class MaterialsTransparencyController : MonoBehaviour
+  {
+    // Editor fields
+
+    [SerializeField]
+    private Material[] materials;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float transparency = 1f;
+
+    // Methods
+
+    protected void OnValidate()
+    {
+      foreach (var material in materials)
+      {
+        material.color = new Color(material.color.r, material.color.g, material.color.b, transparency);
+      }
+    }
+  }
+}
