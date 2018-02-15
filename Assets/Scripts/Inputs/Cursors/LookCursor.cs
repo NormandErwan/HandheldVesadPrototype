9278cbd976332a2813ed7fc6a7c1bed90c096df0
Erwan Normand <normand.erwan@protonmail.com>
2018-02-15 10:49:47 +0100

    Added a basic ready / tap feature in LookCursor.cs

--- Assets/Scripts/Inputs/Cursors/LookCursor.cs
+++ Assets/Scripts/Inputs/Cursors/LookCursor.cs
@@ -1,4 +1,5 @@
 ﻿using NormandErwan.MasterThesis.Experiment.Inputs.Interactables;
+using System;
 using UnityEngine;
 
 namespace NormandErwan.MasterThesis.Experiment.Inputs.Cursors
@@ -11,12 +12,27 @@ namespace NormandErwan.MasterThesis.Experiment.Inputs.Cursors
     [SerializeField]
     private Renderer mesh;
 
-    // ICursor properties
+    // Properties
 
     public override CursorType Type { get { return CursorType.Look; } set { var dummy = value; } }
+    public bool IsReady { get; protected set; }
+
+    // Events
+
+    public event Action<LookCursor> Ready = delegate { };
+    public event Action<LookCursor> Tapped = delegate { };
 
     // Methods
 
+    public void SetReady(bool value)
+    {
+      IsReady = true;
+      Ready(this);
+    }
 
+    public void SetTapped()
+    {
+      Tapped(this);
+    }
   }
 }
--- Assets/Scripts/Inputs/LookCursorInput.cs
+++ Assets/Scripts/Inputs/LookCursorInput.cs
@@ -6,6 +6,11 @@ namespace NormandErwan.MasterThesis.Experiment.Inputs
 {
   public class LookCursorInput : CursorsInput<LookCursor>
   {
+    // Constants
+
+    public const float tapTimeout = 0.5f; // In seconds
+    public const float minDistanceToPinch = 20f;
+
     // Editor fields
 
     [SerializeField]
@@ -25,11 +30,13 @@ namespace NormandErwan.MasterThesis.Experiment.Inputs
 
     // Propreties
 
-    public bool TapWithRightHand { get; set; }
+    public bool ParticipantIsRightHanded { get; set; }
 
     // Variables
 
-    protected Vector3 defaultScale;
+    protected Vector3 defaulLookCursorScale = Vector3.zero;
+    protected float lastReadyTime = 0;
+    protected float lastPinchTime = 0;
 
     // Methods
 
@@ -38,14 +45,46 @@ namespace NormandErwan.MasterThesis.Experiment.Inputs
       base.Awake();
 
       Cursors.Add(lookCursor.Type, lookCursor);
-      defaultScale = lookCursor.transform.localScale;
+      defaulLookCursorScale = lookCursor.transform.localScale;
     }
 
     protected override void UpdateCursors()
     {
+      foreach (var hand in leapServiceProvider.CurrentFrame.Hands)
+      {
+        if (hand.IsRight == ParticipantIsRightHanded)
+        {
+          if (hand.GetIndex().IsExtended && hand.GetThumb().IsExtended)
+          {
+            if (!lookCursor.IsReady)
+            {
+              if (lastPinchTime - lastReadyTime < tapTimeout)
+              {
+                lookCursor.SetTapped();
+              }
+
+              lookCursor.SetReady(true);
+            }
+            lastReadyTime = Time.time;
+          }
+          else
+          {
+            if (lookCursor.IsReady)
+            {
+              lookCursor.SetReady(false);
+            }
+
+            if (hand.PinchDistance < minDistanceToPinch)
+            {
+              lastPinchTime = Time.time;
+            }
+          }
+        }
+      }
+
       lookCursor.transform.position = head.position + defaultLookCursorDistance * head.forward;
       lookCursor.transform.LookAt(head);
-      lookCursor.transform.localScale = Vector3.Distance(head.position, lookCursor.transform.position) / defaultLookCursorDistance * defaultScale;
+      lookCursor.transform.localScale = Vector3.Distance(head.position, lookCursor.transform.position) / defaultLookCursorDistance * defaulLookCursorScale;
 
       if (lookCursor.IsActive)
       {
